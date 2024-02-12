9d5bdf3264960f772d6d49c5e4c858ff0c90939e
remove158 <remove158@gmail.com>
2024-02-13 01:15:16 +0700

    update: treesitter require

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "go", "python" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },

d6e240c15df297cdcde9ccf6060d5fb79cff0010
remove158 <remove158@gmail.com>
2024-02-13 02:09:27 +0700

    add: lazygit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,14 +5,21 @@ return {
     config = function()
       local config = require("toggleterm")
       config.setup({
-        open_mapping = [[<c-t>]],
+        open_mapping = [[<leader>t]],
         shade_filetypes = {},
         direction = 'float',
         float_opts = {
           border = 'curved'
         }
       })
-
+      local Terminal  = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+      
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+      
+      vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
     end
   }
 }

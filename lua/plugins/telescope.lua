33b65212803b98e299fa0c2c77f321eda849db55
remove158 <remove158@gmail.com>
2024-02-12 22:02:12 +0700

    update: readme font for lualine

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>pe", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
       vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 
       require("telescope").load_extension("ui-select")

2952a2546325a0e18d83b68265d4af244bdd20aa
remove158 <remove158@gmail.com>
2024-02-12 22:44:09 +0700

    add: toggle term

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,9 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set nu rnu")
 
+-- clipboard
+vim.cmd("set clipboard+=unnamedplus")
+
 
 -- leader
 vim.g.mapleader = " "
@@ -18,3 +21,8 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 -- Ex
 vim.keymap.set("n", "<leader>e", ":Ex<CR>")
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+
+-- Terminal
+vim.keymap.set("n", "<C-t>", ":ToggleTerm<CR>")
+

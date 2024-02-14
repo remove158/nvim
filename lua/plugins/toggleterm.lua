b8b9313f2a2807cdebb88d8deabf5efbe6c9254a
remove158 <remove158@gmail.com>
2024-02-14 18:34:51 +0700

    fix: git keybinding

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,25 +1,30 @@
 return {
-  {
-    'akinsho/toggleterm.nvim', 
-    version = "*", 
-    config = function()
-      local config = require("toggleterm")
-      config.setup({
-        open_mapping = [[<C-t>]],
-        shade_filetypes = {},
-        direction = 'float',
-        float_opts = {
-          border = 'curved'
-        }
-      })
-      local Terminal  = require('toggleterm.terminal').Terminal
-      local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-      
-      function _lazygit_toggle()
-        lazygit:toggle()
-      end
-      
-      vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
-    end
-  }
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			local config = require("toggleterm")
+			config.setup({
+				open_mapping = [[<C-t>]],
+				shade_filetypes = {},
+				direction = "float",
+				float_opts = {
+					border = "curved",
+				},
+			})
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+			function _lazygit_toggle()
+				lazygit:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<C-g>",
+				"<cmd>lua _lazygit_toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
 }

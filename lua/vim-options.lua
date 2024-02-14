ebb916b22e1be79c1220f30f2cd2b0b9a0d2d21a
remove158 <remove158@gmail.com>
2024-02-14 20:28:21 +0700

    add: moveline map

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,8 @@
 -- vim ui
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set nu rnu")
 
 -- clipboard
@@ -18,6 +18,11 @@ vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
+
 -- Ex
 vim.keymap.set("n", "<leader>e", ":Ex<CR>")
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+-- moveline
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv") 

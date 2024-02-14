dc1a9e51e67d9774966a4804145402448604c68a
remove158 <remove158@gmail.com>
2024-02-15 04:54:57 +0700

    add: comment and format on save

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -7,6 +7,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
     end
 }
 )
+vim.api.nvim_create_augroup('AutoFormatting', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.*',
+    group = 'AutoFormatting',
+    callback = function()
+        vim.lsp.buf.format({ async = true })
+    end,
+})

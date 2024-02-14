45c04c2e5787a0cdee3892d52811ef63fd6e0f1d
remove158 <remove158@gmail.com>
2024-02-15 05:37:55 +0700

    add: icon for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -12,7 +12,8 @@ return {
         "L3MON4D3/LuaSnip",
         dependencies = {
             "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets"
+            "rafamadriz/friendly-snippets",
+            "onsails/lspkind.nvim"
         }
     },
 
@@ -20,8 +21,21 @@ return {
         "hrsh7th/nvim-cmp",
         config = function()
             local cmp = require('cmp')
+            local lspkind = require('lspkind')
             require("luasnip.loaders.from_vscode").lazy_load()
             cmp.setup({
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = "symbol_text",
+                        menu = ({
+                            buffer = "[Buffer]",
+                            nvim_lsp = "[LSP]",
+                            luasnip = "[LuaSnip]",
+                            nvim_lua = "[Lua]",
+                            latex_symbols = "[Latex]",
+                        })
+                    }),
+                },
                 snippet = {
                     expand = function(args)
                         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.

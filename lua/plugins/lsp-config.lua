78917c8a7f55a4d08e63fc3cabf1b1fcf9531944
remove158 <remove158@gmail.com>
2024-02-15 01:09:48 +0700

    add: lsp signature

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,9 +3,11 @@ return {
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
         "williamboman/mason.nvim",
+        "ray-x/lsp_signature.nvim",
     },
     config = function()
         require('mason').setup()
+        require('lsp_signature').setup()
         require('mason-lspconfig').setup({
             ensure_installed = {
                 -- lua

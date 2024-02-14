7ecc5e6f5ba252bbc65558d0507e07da5993392c
remove158 <remove158@gmail.com>
2024-02-14 21:48:26 +0700

    add: lsp-config with ensur install list

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,7 +6,23 @@ return {
     },
     config = function()
         require('mason').setup()
-        require('mason-lspconfig').setup()
+        require('mason-lspconfig').setup({
+            ensure_installed = {
+                -- lua
+                "lua_ls",
+                -- typescript
+                "tsserver",
+                "eslint",
+                -- golang
+                "gopls",
+                "golangci_lint_ls",
+            },
+            handlers =  {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {}
+                end
+            }
+        })
 
     end
 }

4750c49b99dfbc4e288097098c17f6f630da2737
remove158 <remove158@gmail.com>
2024-02-15 07:35:19 +0700

    fix: tab

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,11 +1,10 @@
 -- vim ui
-vim.cmd("set expandtab")
+vim.cmd("set list")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nu rnu")
 
+
 -- clipboard
 vim.cmd("set clipboard+=unnamedplus")
-
-

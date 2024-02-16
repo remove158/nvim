98b4ff9342fff249da57c3028a6c62a136dbdd47
remove158 <remove158@gmail.com>
2024-02-16 21:43:43 +0700

    add: code action

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -6,6 +6,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		-- vim.keymap.set({ "i", "n" }, "<C-h>", vim.lsp.buf.signature_help, opts)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
 
 		vim.keymap.set('n', '<space>f', function()

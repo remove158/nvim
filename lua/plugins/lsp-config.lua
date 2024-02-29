007babb90a893ca9e2d6110d519cf470893e97d9
remove158 <remove158@gmail.com>
2024-03-01 04:13:15 +0700

    update: nvim autocmd

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -50,7 +50,7 @@ return {
 			callback = function(ev)
 				local opts = { buffer = ev.buf }
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-				-- vim.keymap.set({ "i", "n" }, "<C-h>", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set({ "i", "n" }, "<C-.>", vim.lsp.buf.signature_help, opts)
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)

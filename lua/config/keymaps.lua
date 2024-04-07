06f861624d97c664fe9e3f8f8a8c5e28ddf12f67
remove158 <remove158@gmail.com>
2024-04-08 03:39:36 +0700

    fix: lazyvim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+--  nohlsearch
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+-- Page scroll
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<C-g>", ":LazyGit<CR>")

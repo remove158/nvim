4468ea362535fee293124d773ae37d689d300682
Piyaphat Pinyo <remove158@192.168.2.40>
2024-04-15 05:31:08 +0700

    update: lazygit

--- lua/remove158/plugins/lazygit.lua
+++ lua/remove158/plugins/lazygit.lua
@@ -1,19 +1,25 @@
 return {
-  "kdheepak/lazygit.nvim",
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
-  },
-  -- optional for floating window border decoration
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { "<C-g>", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
-  },
+	"kdheepak/lazygit.nvim",
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFloat",
+		"LazyGitFilterCurrentFile",
+	},
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	-- setting the keybinding for LazyGit with 'keys' is recommended in
+	-- order to load the plugin when the command is run for the first time
+	keys = {
+		{ "<C-g>", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+	},
+	config = function()
+		require("telescope").load_extension("lazygit")
+		vim.g.lazygit_floating_window_winblend = 1
+	end,
 }

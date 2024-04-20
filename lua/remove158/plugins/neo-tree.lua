25c3de041e459bbcace3f85a9f316898848fe364
Piyaphat Pinyo <remove158@192.168.2.41>
2024-04-20 07:29:15 +0700

    update: neotree

--- lua/remove158/plugins/neo-tree.lua
+++ lua/remove158/plugins/neo-tree.lua
@@ -8,9 +8,23 @@ return {
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	keys = {
-		{ "<leader>e", "<cmd>Neotree toggle reveal<cr>", desc = "Toggle Neo-tree" },
+		{ "<leader>e", "<cmd>Neotree reveal<CR>", desc = "Open Neo-tree files" },
 	},
 	config = {
+		source_selector = {
+			winbar = true,
+		},
+		filesystem = {
+			filtered_items = {
+				always_show = { -- remains visible even if other settings would normally hide it
+					".gitignore",
+					".env",
+				},
+			},
+			follow_current_file = {
+				enabled = true, -- This will find and focus the file in the active buffer every time
+			},
+		},
 		window = {
 			mappings = {
 				["?"] = { "show_help", config = { prefix_key = "q" } },

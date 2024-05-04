c00085b24f3e52410112ab7c08051c9f9f5bdb87
remove158 <remove158@gmail.com>
2024-05-04 19:35:13 +0700

    update: telescope ui

--- lua/remove158/plugins/telescope.lua
+++ lua/remove158/plugins/telescope.lua
@@ -7,14 +7,6 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
-	opts = {
-		defaults = {
-			layout_strategy = "horizontal",
-			layout_config = { prompt_position = "top" },
-			sorting_strategy = "ascending",
-			winblend = 0,
-		},
-	},
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
@@ -32,6 +24,51 @@ return {
 
 		telescope.setup({
 			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"-L",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+				},
+				prompt_prefix = "   ",
+				selection_caret = "  ",
+				entry_prefix = "  ",
+				initial_mode = "insert",
+				selection_strategy = "reset",
+				sorting_strategy = "ascending",
+				layout_strategy = "horizontal",
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.55,
+						results_width = 0.8,
+					},
+					vertical = {
+						mirror = false,
+					},
+					width = 0.87,
+					height = 0.80,
+					preview_cutoff = 120,
+				},
+				file_sorter = require("telescope.sorters").get_fuzzy_file,
+				file_ignore_patterns = { "node_modules" },
+				generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+				path_display = { "truncate" },
+				winblend = 0,
+				border = {},
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				color_devicons = true,
+				set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+				file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+				grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+				qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+				-- Developer configurations: Not meant for general override
+				buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -53,5 +90,26 @@ return {
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
+		local colors = require("catppuccin.palettes").get_palette()
+		local TelescopeColor = {
+			TelescopeMatching = { fg = colors.flamingo },
+			TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
+
+			TelescopePromptPrefix = { bg = colors.surface0 },
+			TelescopePromptNormal = { bg = colors.surface0 },
+			TelescopeResultsNormal = { bg = colors.mantle },
+			TelescopePreviewNormal = { bg = colors.mantle },
+			TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+			TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+			TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
+			TelescopePromptTitle = { bg = colors.pink, fg = colors.mantle },
+			TelescopeResultsTitle = { fg = colors.mantle },
+			TelescopePreviewTitle = { bg = colors.green, fg = colors.mantle },
+		}
+
+		for hl, col in pairs(TelescopeColor) do
+			vim.api.nvim_set_hl(0, hl, col)
+		end
 	end,
 }
